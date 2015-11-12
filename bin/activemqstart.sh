ab07f3dab739e62357041f583ddd4344b5e6c62f
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2015-11-12 15:29:27 -0600

    Removed out of data startup and shutdown scripts. Add configuration file that does garbage collection on old topics and that will cache messages to disk if the broker gets backed up.

--- bin/activemqstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# $JAVA_HOME is set when "setup java" is run
-export JDK_HOME=$JAVA_HOME
-# $ACTIVEMQ_DIR is set when "setup activemq" is run
-ACTHOME=$ACTIVEMQ_DIR
-$ACTIVEMQ_DIR/bin/activemq xbean:file:$CTRL_ACTIVEMQ_DIR/conf/lsstactivemq.xml >$ACTIVEMQ_DIR/log/activemq.log &
